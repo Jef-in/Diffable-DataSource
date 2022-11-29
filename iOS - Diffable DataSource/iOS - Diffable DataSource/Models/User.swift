c35a19f86aa49dc90c81af6d07543d6c4e7d6aad
Jef-in <jefinabduljaleel@gmail.com>
2022-11-29 18:46:29 +0530

    user model created

---
+++ iOS - Diffable DataSource/iOS - Diffable DataSource/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  iOS - Diffable DataSource
+//
+//  Created by Jefin on 29/11/22.
+//
+
+struct User {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+}
--- iOS - Diffable DataSource/iOS - Diffable DataSource/Views/UserListViewController.swift
+++ iOS - Diffable DataSource/iOS - Diffable DataSource/Views/UserListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UserListViewController.swift
 //  iOS - Diffable DataSource
 //
 //  Created by Jefin on 29/11/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UserListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
