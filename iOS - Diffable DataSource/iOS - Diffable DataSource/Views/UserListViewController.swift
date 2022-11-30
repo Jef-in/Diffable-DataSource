3d08c4e6f7815518e9e90d523a107c6c8beaef2e
Jef-in <jefinabduljaleel@gmail.com>
2022-11-30 18:33:56 +0530

    userTable created

--- iOS - Diffable DataSource/iOS - Diffable DataSource/Views/UserListViewController.swift
+++ iOS - Diffable DataSource/iOS - Diffable DataSource/Views/UserListViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class UserListViewController: UIViewController {
 
+    private lazy var userTable: UITableView = {
+       let tableView = UITableView()
+       tableView.frame = view.bounds
+       return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupUserTable()
     }
 
+    func setupUserTable() {
+        view.addSubview(userTable)
+    }
 
 }
 
